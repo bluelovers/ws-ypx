5abc34b9a3138f31ba076ac6f71526012be014eb
bluelovers <codelovers@users.sourceforge.net>
2020-01-29 21:30:02 +0800

    test: add test install

--- packages/ypx/test/install.test.ts
+++ packages/ypx/test/install.test.ts
@@ -26,14 +26,6 @@ test(`test install`, async () =>
 	});
 
 	await crossSpawnExtra('yarn', [
-		'add',
-		'ynpx',
-	], {
-		stdio: 'inherit',
-		cwd,
-	});
-
-	await crossSpawnExtra('yarn', [
 		'run',
 		'ynpx',
 		'--debug-bin'
@@ -48,9 +40,55 @@ test(`test install`, async () =>
 			//console.dir(cp);
 
 			// @ts-ignore
-			//expect(cp.exitCode).not.toEqual(1);
+			console.log(`cp.exitCode =>`, cp.exitCode);
+			// @ts-ignore
+			expect(cp.exitCode).not.toEqual(1);
 
+			console.log(`output =>`, output);
 			expect(output).toContain('ypx_');
+
+		})
+	;
+
+	await actual.remove();
+
+});
+
+test(`cowsay`, async () =>
+{
+	let actual = await newTemporary();
+
+	let cwd = actual.tmpDir;
+
+	console.log(`target => `, cwd);
+
+	await crossSpawnExtra('yarn', [
+		'add',
+		'ynpx',
+	], {
+		cwd,
+	});
+
+	await crossSpawnExtra('yarn', [
+		'run',
+		'ynpx',
+		'cowsay',
+		'--',
+		'-q',
+		'--',
+		'test'
+	], {
+		cwd,
+		stripAnsi: true,
+	})
+		.then(cp => {
+
+			let output = cp.stdout.toString().replace(/^\s+|\s+$/g,'');
+
+			console.log(output);
+
+			expect(output).toContain('< test >');
+			expect(output).toContain('(oo)\\_______');
 
 		})
 	;
